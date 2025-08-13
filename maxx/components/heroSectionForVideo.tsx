be5bd1b9d6003c4d4d3a4b85999398467d68d151
kiran04-code <kr551344@gmail.com>
2025-08-13 18:05:54 +0530

    work on herosection

--- maxx/components/heroSectionForVideo.tsx
+++ maxx/components/heroSectionForVideo.tsx
@@ -5,7 +5,7 @@ const HeroSectionForVideo = () => {
     return (
         <div className='md:flex md:justify-between '>
             <div className='w-full md:w-[800px] md:p-20 p-5 flex flex-col gap-5'>
-                <h1 className='text-4xl sm:text-5xl md:text-[80px] font-bold leading-snug md:leading-[1.1]'>
+                <h1 className='text-4xl sm:text-5xl md:text-[80px] font-bold leading-snug md:leading-[1.2]'>
                     Thrifted Finds.{' '}
                     <span className='bg-black text-white px-3 sm:px-5 md:px-8'>
                         Timeless
@@ -20,7 +20,7 @@ const HeroSectionForVideo = () => {
 
             <div className='retaive  justify-end  items-center flex'>
                 <div className='absoulte '>
-                    <video src="kiran01.mp4 " className=' border-2 rounded-2xl shadow-lg' loop autoPlay muted></video>
+                    <video src="kiran01.mp4 " className='rounded-3xl shadow-2xl border-2 ' loop autoPlay muted></video>
                 </div>
 
             </div>
