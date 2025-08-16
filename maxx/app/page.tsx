e10defd61bbab51a93b2e9dcccc249c02f3fe60d
kiran04-code <kr551344@gmail.com>
2025-08-16 18:44:49 +0530

    title

--- maxx/app/page.tsx
+++ maxx/app/page.tsx
@@ -13,10 +13,13 @@ import ImageSections from "@/components/image";
 import Footer2 from "@/components/Footer2";
 import Cloth from "@/components/bradingComponents/Cloth";
 import MarQuee from "@/components/MarQuee";
+import Sell from "@/components/sellSection/sell";
 export default function Home() {
 
   return (
+    
     <div className="flex-col  ">
+    
       <div className="md:flex md:justify-between flex justify-between md:px-20 md:py-5 px-5 py-2 ">
         <div>
           <Nvbar2 />
@@ -35,8 +38,9 @@ export default function Home() {
       <div className="mt-20 md:flex md:justify-start md:px-20 px-2">
         <AnimationSider />
       </div>
-
+       
       <div className="md:flex mt-10 md:justify-start md:px-20 gap-5">
+        <Sell/>
         <FaqSection />
         <Cloth/>
       </div>
---
+++ maxx/components/sellSection/sell.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sell = () => {
+  return (
+    <div>
+      <h1 className='text-5xl'>Sell</h1>
+    </div>
+  )
+}
+
+export default Sell
